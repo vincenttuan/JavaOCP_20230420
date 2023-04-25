811fcedecf61543a0aa126e5e085785655830f65
MB-teacher <MB-teacher@MB-207-teacher>
2023-04-25 20:52:01 +0800

    java

---
+++ src/day02/Coffee.java
@@ -0,0 +1,29 @@
+package day02;
+
+public class Coffee {
+	public static String name; // 咖啡名稱
+	public static double ratioOfMilk; // 牛奶的比例
+	public static int price; // 價格
+	
+	// 調整牛奶比例的方法
+	public static void changeRatioOfMilk(double ratio) {
+		if(ratio == 0) {
+			name = "美式";
+			ratioOfMilk = ratio;
+			price = (int)(45 + 45*(ratio));
+		} else if (ratio == 0.3) {
+			name = "卡布奇諾";
+			ratioOfMilk = ratio;
+			price = (int)(45 + 45*(ratio));
+		} else if (ratio == 0.7) {
+			name = "拿鐵";
+			ratioOfMilk = ratio;
+			price = (int)(45 + 45*(ratio));
+		} else {
+			name = null;
+			ratioOfMilk = 0;
+			price = 0;
+		}
+	}
+	
+}
