424215dafbe40c5c84ec674458c687fa52107adf
MB-teacher <MB-teacher@MB-207-teacher>
2023-04-25 21:07:39 +0800

    java

--- src/day02/CoffeeStore.java
+++ src/day02/CoffeeStore.java
@@ -4,23 +4,30 @@ package day02;
 public class CoffeeStore {
 
 	public static void main(String[] args) {
+		int total = 0;
 		// 設定牛奶比例
 		Coffee.changeRatioOfMilk(0);
+		System.out.println(Coffee.price);
+		total += Coffee.price;
 		// 印出咖啡商品資料
 		Coffee.printCoffee();
 		
 		// 設定牛奶比例
 		Coffee.changeRatioOfMilk(0.3);
+		System.out.println(Coffee.price);
+		total += Coffee.price;
 		// 印出咖啡商品資料
 		Coffee.printCoffee();
 
 		// 設定牛奶比例
 		Coffee.changeRatioOfMilk(0.7);
+		System.out.println(Coffee.price);
+		total += Coffee.price;
 		// 印出咖啡商品資料
 		Coffee.printCoffee();
 		
 		// 請問上面的三杯咖啡總共多少錢 ?
-		
+		System.out.printf("Total: %d\n", total);
 		
 		
 		System.out.println(Math.round(4.5)); // 四捨五入
