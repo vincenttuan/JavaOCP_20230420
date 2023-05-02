84ccb28aaf64a49962e61e6cd6342cc24f145bd1
MB-teacher <MB-teacher@MB-207-teacher>
2023-05-02 21:21:49 +0800

    java

--- src/day04/ArrayDemo6.java
+++ src/day04/ArrayDemo6.java
@@ -5,7 +5,7 @@ import static day04.Util.getCV;
  * 調查五位學生之身高及體重如下
  * 身高：172、168、164、170、176(公分)
  * 體重：62、57、58、64、64(公斤)
- * 試問：平均
+ * 試問：學生的身高,體重哪一個資料較集中 ?
  * */
 public class ArrayDemo6 {
 	public static void main(String[] args) {
