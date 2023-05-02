e0786c0bd0743e59bafe49418fb2c19ee3d9142f
MB-teacher <MB-teacher@MB-207-teacher>
2023-05-02 21:27:11 +0800

    java

--- src/day04/ArrayDemo7.java
+++ src/day04/ArrayDemo7.java
@@ -1,5 +1,6 @@
 package day04;
 
+import static day04.Util.getCV;
 /*
  * 某公司有 18 位員工
  * 其中 10 位在去年投資股票，一年的獲利率如下(單位：%)
@@ -12,6 +13,9 @@ public class ArrayDemo7 {
 	public static void main(String[] args) {
 		double[] stockProfitRate = {7.6, 3.9, 15.6, 28.3, 1.2, 10.8, 35.3, 45.6, 10.2, 0.5};
 		double[] bondProfitRate = {6.8, 7.2, 6.8, 7.5, 6.9, 7.9, 7.1, 7.2};
-		
+		double cvOfStock = getCV(stockProfitRate);
+		double cvOfBond = getCV(bondProfitRate);
+		System.out.println(cvOfStock);
+		System.out.println(cvOfBond);
 	}
 }
