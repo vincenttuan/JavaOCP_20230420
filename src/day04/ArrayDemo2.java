b99106951601591e598c490c0b751d5c706e77d6
MB-teacher <MB-teacher@MB-207-teacher>
2023-05-02 19:11:47 +0800

    java

--- src/day04/ArrayDemo2.java
+++ src/day04/ArrayDemo2.java
@@ -8,7 +8,15 @@ package day04;
  * */
 public class ArrayDemo2 {
 	public static void main(String[] args) {
-		
-		
+		double[] heights = {172, 168, 164, 170, 176};
+		double[] weights = {62, 57, 58, 64, 64};
+		// 計算身高的平均
+		// 先計算身高的總和
+		double sumOfHeight = 0;
+		for(double height : heights) {
+			sumOfHeight += height;
+		}
+		double avgOfHeight = sumOfHeight / heights.length;
+		System.out.printf("身高總和: %.1f 身高平均: %.1f\n", sumOfHeight, avgOfHeight);
 	}
 }
