daaf71127acedbbfb0075c518b0f9157594be596
MB-teacher <MB-teacher@MB-207-teacher>
2023-05-02 19:58:38 +0800

    java

--- src/day04/ArrayDemo3.java
+++ src/day04/ArrayDemo3.java
@@ -10,7 +10,12 @@ package day04;
  * */
 public class ArrayDemo3 {
 	public static void main(String[] args) {
+		double[] stockProfitRate = {7.6, 3.9, 15.6, 28.3, 1.2, 10.8, 35.3, 45.6, 10.2, 0.5};
+		double avgStockProfitRate = Util.getAvg(stockProfitRate);
+		System.out.printf("平均股票獲利率: %.1f%%\n", avgStockProfitRate);
 		
-		
+		double[] bondProfitRate = {6.8, 7.2, 6.8, 7.5, 6.9, 7.9, 7.1, 7.2};
+		double avgBondProfitRate = Util.getAvg(bondProfitRate);
+		System.out.printf("平均債券獲利率: %.1f%%\n", avgBondProfitRate);
 	}
 }
