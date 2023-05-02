b3e5e58f8b46e168619b3fbd39726f469598c32e
MB-teacher <MB-teacher@MB-207-teacher>
2023-05-02 19:38:23 +0800

    java

--- src/day04/ArrayDemo2.java
+++ src/day04/ArrayDemo2.java
@@ -1,11 +1,11 @@
 package day04;
 
-import static day04.Util.getAvg;
+//import static day04.Util.getAvg;
 /*
  * 調查五位學生之身高及體重如下
  * 身高：172、168、164、170、176(公分)
  * 體重：62、57、58、64、64(公斤)
- * 試問：平均與標準差
+ * 試問：平均
  * */
 public class ArrayDemo2 {
 	public static void main(String[] args) {
@@ -31,10 +31,10 @@ public class ArrayDemo2 {
 		System.out.printf("體重總和: %.1f 體重平均: %.1f\n", sumOfWeight, avgOfWeight);
 		//-----------------------------------------------------------------------------
 		// 透過 getAvg() 方法來取得身高的平均
-		avgOfHeight = getAvg(heights);
+		avgOfHeight = Util.getAvg(heights);
 		System.out.printf("身高的平均: %.1f\n", avgOfHeight);
 		// 透過 getAvg() 方法來取得體重的平均
-		avgOfWeight = getAvg(weights);
+		avgOfWeight = Util.getAvg(weights);
 		System.out.printf("體重的平均: %.1f\n", avgOfWeight);
 	}
 	
