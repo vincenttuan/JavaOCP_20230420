8e6930109ec366120cdb5387b2dcc6d9c6bd6a92
MB-teacher <MB-teacher@MB-207-teacher>
2023-05-04 21:28:15 +0800

    java

--- src/day05/ArrayDemo6.java
+++ src/day05/ArrayDemo6.java
@@ -1,9 +1,15 @@
 package day05;
 
+import java.util.Arrays;
+
 public class ArrayDemo6 {
 	public static void main(String[] args) {
 		String[] names = {"John", "Vincent", "Anita", "Jo"};
 		// 題目:計算名字的平均字母有幾個 (使用 Java 8 stream)
-		
-	}
+		double avg = Arrays.stream(names)  // ["John", "Vincent", "Anita", "Jo"]
+						   .mapToInt(x -> x.length()) // [4, 7, 5, 2]
+						   .average()
+						   .getAsDouble();
+		System.out.println(avg);
+	}					   
 }
