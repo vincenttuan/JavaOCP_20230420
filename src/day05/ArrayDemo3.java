f1482e031d5f47bce30b29f938befd62e7010adf
MB-teacher <MB-teacher@MB-207-teacher>
2023-05-04 19:32:15 +0800

    java

---
+++ src/day05/ArrayDemo3.java
@@ -0,0 +1,10 @@
+package day05;
+
+public class ArrayDemo3 {
+	public static void main(String[] args) {
+		// 分數 -1 表示缺考
+		int[] scores = {100, 50, -1, 40, 70, -1, 85, -1};
+		// 有考試成績的學生有幾位(不含缺考) ? 平均(不含缺考) = ?
+		
+	}
+}
