ce5507446ff92951b7e3b22729dd623c915c5446
MB-teacher <MB-teacher@MB-207-teacher>
2023-05-04 21:39:20 +0800

    java

--- src/day05/ArrayDemo7.java
+++ src/day05/ArrayDemo7.java
@@ -8,8 +8,8 @@ public class ArrayDemo7 {
 	public static void main(String[] args) {
 		String[] names = {"John", "Vincent", null, "Anita", "Jo", null};
 		// 題目:計算每個有效名字的字母總和與平均 (使用 Java 8 stream)
-		IntSummaryStatistics stat = Arrays.stream(names)  // {"John", "Vincent", null, "Anita", "Jo", null}
-										  .filter(x -> x != null)  // {"John", "Vincent", "Anita", "Jo"}
+		IntSummaryStatistics stat = Arrays.stream(names)             // {"John", "Vincent", null, "Anita", "Jo", null}
+										  .filter(x -> x != null)    // {"John", "Vincent", "Anita", "Jo"}
 										  .mapToInt(x -> x.length()) // {4, 7, 5, 2}
 										  .summaryStatistics();
 		System.out.println(stat.getSum());
