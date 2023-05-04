2b96ca0b747850a33d1c29ea97bddc1e1eb864f8
MB-teacher <MB-teacher@MB-207-teacher>
2023-05-04 20:40:45 +0800

    java

---
+++ src/day05/OptionalDemo.java
@@ -0,0 +1,15 @@
+package day05;
+
+public class OptionalDemo {
+
+	public static void main(String[] args) {
+		// Optional 可能包含也可能不包含容器
+		// -1 表示缺考
+		int[] scores = {40, -1, 50, 35, -1};
+		// 請計算出及格分數的平均值(不含缺考)
+		
+		
+
+	}
+
+}
