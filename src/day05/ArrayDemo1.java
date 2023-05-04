3e95b207c98c8d9b2c9e0c359bf9e9efca665742
MB-teacher <MB-teacher@MB-207-teacher>
2023-05-04 19:14:38 +0800

    java

--- src/day05/ArrayDemo1.java
+++ src/day05/ArrayDemo1.java
@@ -1,5 +1,7 @@
 package day05;
 
+import java.util.Arrays;
+
 public class ArrayDemo1 {
 
 	public static void main(String[] args) {
@@ -7,10 +9,15 @@ public class ArrayDemo1 {
 		// 請印出及格的分數
 		// for-each
 		for(int x : scores) {
-			if(x >= 60) {
+			if(x >= 60) { // 過濾 x 資料
 				System.out.println(x);
 			}
 		}
+		System.out.println("------");
+		// Java 8 Arrays.stream()
+		Arrays.stream(scores)  // 將 int[] 串流化為 IntStream
+			  .filter(x -> x >= 60) // 過濾 x 資料
+			  .forEach(x -> System.out.println(x));
 		
 	}
 
