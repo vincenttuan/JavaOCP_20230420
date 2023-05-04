ade5bc5fc680ce18206004ceeec49d81c4c24feb
MB-teacher <MB-teacher@MB-207-teacher>
2023-05-04 21:34:06 +0800

    java

---
+++ src/day05/ArrayDemo7.java
@@ -0,0 +1,12 @@
+package day05;
+
+public class ArrayDemo7 {
+
+	public static void main(String[] args) {
+		String[] names = {"John", "Vincent", null, "Anita", "Jo", null};
+		// 題目:計算每個有效名字的字母總和與平均 (使用 Java 8 stream)
+		
+		
+	}
+
+}
