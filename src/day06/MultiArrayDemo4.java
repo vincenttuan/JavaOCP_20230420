7acc678faef7dc45f40791aef19507d2aea2a036
MB-teacher <MB-teacher@MB-207-teacher>
2023-05-09 20:36:46 +0800

    java

--- src/day06/MultiArrayDemo4.java
+++ src/day06/MultiArrayDemo4.java
@@ -1,9 +1,20 @@
 package day06;
 
+import java.util.Arrays;
+
 public class MultiArrayDemo4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[][] mm = {
+				{100, 90},
+				{85, 75, 65},
+				{70, 60}
+			};
+		
+		// 用 Java 8 Stream 來印出每一個元素
+		Arrays.stream(mm) // {{100, 90},{85, 75, 65},{70, 60}}
+			  .flatMapToInt(m -> Arrays.stream(m)) // {100, 90}  {85, 75, 65}  {70, 60}
+			  .forEach(x -> System.out.println(x));
 
 	}
 
---
+++ src/day06/MultiArrayDemo5.java
@@ -0,0 +1,16 @@
+package day06;
+
+public class MultiArrayDemo5 {
+
+	public static void main(String[] args) {
+		// 有三組{身高, 體重}的組合
+		double[][] mm = {
+				{170, 60},
+				{180, 80},
+				{165, 55}
+			};
+		// 請求出每一組的 bmi 資料
+		
+	}
+
+}
