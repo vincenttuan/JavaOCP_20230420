9e6d9714336dd226c1f948e3692ccaa3d477dab0
MB-teacher <MB-teacher@MB-207-teacher>
2023-05-23 21:33:06 +0800

    java

--- src/day07/Bread.java
+++ src/day07/Bread.java
@@ -3,7 +3,7 @@ package day07;
 public class Bread {
 	String type; // 麵包種類
 	int weight; // 麵包重量(克)
-	int pricePerGram; // 每克麵包的價格(單位:新台幣)
+	static int pricePerGram = 3; // 每克麵包的價格(單位:新台幣 統一定價)
 	
 	int getPrice() { // 麵包價格
 		return weight * pricePerGram;
--- src/day07/BreadMain.java
+++ src/day07/BreadMain.java
@@ -1,15 +1,31 @@
 package day07;
 
+import java.util.Arrays;
+
 public class BreadMain {
 	public static void main(String[] args) {
+		Bread.pricePerGram = 8;
+		
 		Bread b1 = new Bread();
-		b1.type="法國麵包";b1.weight=100;b1.pricePerGram=2;
+		b1.type="法國麵包";b1.weight=100;
 		Bread b2 = new Bread();
-		b2.type="小餐包";b2.weight=5;b2.pricePerGram=4;
+		b2.type="小餐包";b2.weight=5;
 		Bread b3 = new Bread();
-		b3.type="油條麵包";b3.weight=15;b3.pricePerGram=2;
+		b3.type="油條麵包";b3.weight=15;
 		
 		// 請問我要買 3 個法國麵包, 2 個小餐包, 4 個油條麵包 共需要多少錢 ?
+		Bread[] breads = {b1, b1, b1, b2, b2, b3, b3, b3, b3};
+		int sum = 0;
+		for(Bread bread: breads) {
+			sum += bread.getPrice();
+		}
+		System.out.println(sum);
+		
+		// java 8		
+		int sum2 = Arrays.stream(breads)
+						 .mapToInt(bread -> bread.getPrice())
+						 .sum();
+		System.out.println(sum2);
 		
 		
 	}
