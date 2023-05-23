3f763e15f3294c1f9b7514ffc8b1b224c9828ea0
MB-teacher <MB-teacher@MB-207-teacher>
2023-05-23 20:59:19 +0800

    java

---
+++ src/day07/BreadMain.java
@@ -0,0 +1,16 @@
+package day07;
+
+public class BreadMain {
+	public static void main(String[] args) {
+		Bread b1 = new Bread();
+		b1.type="法國麵包";b1.weight=100;b1.pricePerGram=2;
+		Bread b2 = new Bread();
+		b2.type="小餐包";b2.weight=5;b2.pricePerGram=4;
+		Bread b3 = new Bread();
+		b3.type="油條麵包";b3.weight=15;b3.pricePerGram=2;
+		
+		// 請問我要買 3 個法國麵包, 2 個小餐包, 4 個油條麵包 共需要多少錢 ?
+		
+		
+	}
+}
