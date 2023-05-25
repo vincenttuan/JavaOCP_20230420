15d52456ca13af218736b92c730130ad51c2762c
MB-teacher <MB-teacher@MB-207-teacher>
2023-05-25 19:05:47 +0800

    java

---
+++ src/day08/Account.java
@@ -0,0 +1,24 @@
+package day08;
+
+public class Account {
+	
+	String name; // 戶名
+	int balance; // 餘額
+	
+	void setName(String name) { // 設定戶名
+		
+	}
+	
+	void deposit(int amount) { // 存款
+		
+	}
+	
+	void withdraw(int amount) { // 提款
+		
+	}
+	
+	void print() { // 列印目前餘額資料
+		
+	}
+	
+}
