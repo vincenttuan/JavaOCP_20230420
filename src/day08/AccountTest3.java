c05a835f066ad3a3cb50405259da9cec5c8e2be9
MB-teacher <MB-teacher@MB-207-teacher>
2023-05-25 20:00:10 +0800

    java

--- src/day08/AccountTest3.java
+++ src/day08/AccountTest3.java
@@ -10,14 +10,23 @@ public class AccountTest3 {
 		act2.setName("Mary");act2.deposit(200);
 		Account act3 = new Account();
 		act3.setName("Helen");act3.deposit(300);
-		// 逐筆印出帳戶內容-case 1:
+		
 		Account[] accounts = {act1, act2, act3};
+		
+		// 逐筆印出帳戶內容-case 1:
+		for(int i=0;i<accounts.length;i++) {
+			accounts[i].print();
+		}
+		
+		// 逐筆印出帳戶內容-case 2:
 		for(Account act: accounts) {
 			act.print();
 		}
-		// 逐筆印出帳戶內容-case 2:
-		Arrays.stream(accounts).forEach(act -> act.print());
+		
 		// 逐筆印出帳戶內容-case 3:
+		Arrays.stream(accounts).forEach(act -> act.print());
+		
+		// 逐筆印出帳戶內容-case 4:
 		Arrays.stream(accounts).forEach(Account::print);
 		
 	}
