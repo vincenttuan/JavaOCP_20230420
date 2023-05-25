2e26b37c1f60f865cc2ac4b69d7d4afec7b9df59
MB-teacher <MB-teacher@MB-207-teacher>
2023-05-25 20:33:23 +0800

    java

---
+++ src/day08/Color.java
@@ -0,0 +1,14 @@
+package day08;
+
+public class Color {
+	private String colorName; // 顏色的名字
+
+	public String getColorName() {
+		return colorName;
+	}
+
+	public void setColorName(String colorName) {
+		this.colorName = colorName;
+	}
+	
+}
---
+++ src/day08/Size.java
@@ -0,0 +1,16 @@
+package day08;
+
+public class Size {
+	
+	private String sizeName; // 尺寸名稱
+
+	public String getSizeName() {
+		return sizeName;
+	}
+
+	public void setSizeName(String sizeName) {
+		this.sizeName = sizeName;
+	}
+	
+	
+}
