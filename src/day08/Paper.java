cc6dd463f1c6fdcae66b4890a32ec71609088d67
MB-teacher <MB-teacher@MB-207-teacher>
2023-05-25 20:39:06 +0800

    java

---
+++ src/day08/Paper.java
@@ -0,0 +1,29 @@
+package day08;
+
+public class Paper {
+	
+	private int price; // 價格 
+	private Color color; // 顏色
+	private Size size; // 尺寸
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public Color getColor() {
+		return color;
+	}
+	public void setColor(Color color) {
+		this.color = color;
+	}
+	public Size getSize() {
+		return size;
+	}
+	public void setSize(Size size) {
+		this.size = size;
+	}
+	
+	
+	
+}
