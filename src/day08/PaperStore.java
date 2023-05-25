203b541f0a01ef0cf6d403a1fb45233718fd7c83
MB-teacher <MB-teacher@MB-207-teacher>
2023-05-25 20:47:21 +0800

    java

---
+++ src/day08/PaperStore.java
@@ -0,0 +1,40 @@
+package day08;
+
+public class PaperStore {
+
+	public static void main(String[] args) {
+		Color red = new Color();
+		red.setColorName("Red");
+		
+		Color white = new Color();
+		white.setColorName("White");
+		
+		Color yellow = new Color();
+		yellow.setColorName("Yellow");
+		
+		Size a4 = new Size();
+		a4.setSizeName("A4");
+		
+		Size b5 = new Size();
+		b5.setSizeName("B5");
+		
+		// 組合
+		Paper p1 = new Paper();
+		p1.setColor(yellow);p1.setSize(a4);p1.setPrice(10);
+		
+		Paper p2 = new Paper();
+		p2.setColor(white);p2.setSize(b5);p2.setPrice(8);
+		
+		Paper p3 = new Paper();
+		p3.setColor(red);p3.setSize(b5);p3.setPrice(12);
+		
+		Paper p4 = new Paper();
+		p4.setColor(white);p4.setSize(a4);p4.setPrice(15);
+		
+		Paper[] papers = {p1, p2, p3, p4};
+		
+		
+		
+	}
+
+}
