fac8125a0197ae17960c2393f22f199598a20c88
MB-teacher <MB-teacher@MB-207-teacher>
2023-05-30 21:02:15 +0800

    java

---
+++ src/day09/Doneness.java
@@ -0,0 +1,11 @@
+package day09;
+
+// 牛排的熟度
+public interface Doneness { // 介面/規格
+	String BLUE = "Blue"; // ０．５－１分熟
+	String RARE = "Rare"; // １－２分熟
+	String MEDIUM_RARE = "Medium Rare "; // ３－４分熟
+	String MEDIUM = "Medium"; // ５－６分熟
+	String MEDIUM_WELL = "Medium Well"; // ７－８分熟
+	String WELL_DONE = "Well-done"; // ９－１０分熟
+}
