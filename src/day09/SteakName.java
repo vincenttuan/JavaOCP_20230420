bd3db795997a7b8f1055b6cbb0071e52004532a0
MB-teacher <MB-teacher@MB-207-teacher>
2023-05-30 21:09:04 +0800

    java

---
+++ src/day09/SteakName.java
@@ -0,0 +1,13 @@
+package day09;
+
+// 牛排的名稱
+public interface SteakName { // 介面/規格
+	String 丁骨牛排 = "T-Bone 丁骨牛排";
+	String 無骨莎朗 = "Top Sirloin 無骨莎朗";
+	String 牛腹脅肉 = "Flank 牛腹脅肉";
+	String 紐約客牛排 = "New York Strip 紐約客牛排";
+	String 菲力牛排 = "Filet Mignon 菲力牛排";
+	String 肋眼牛排 = "Rib-Eye 肋眼牛排";
+	String 牛小排 = "Beef ribs 牛小排";
+	String 紅屋牛排 = "Porterhouse 紅屋牛排";
+}
