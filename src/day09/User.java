e223cfa4b0dbd9dd64bbb4531d83123bc21b30a2
MB-teacher <MB-teacher@MB-207-teacher>
2023-05-30 19:14:59 +0800

    java

--- src/day09/User.java
+++ src/day09/User.java
@@ -8,13 +8,19 @@ public class User {
 		System.out.println("建構子1");
 	}
 	
-	public User(double h, double w) {
+	public User(double h, double w) { // 建構子封裝
 		System.out.println("建構子2");
 		height = h;
 		weight = w;
 	}
 	
-	// getter/setter 封裝
+	// 自訂商業方法
+	public double getBmi() {
+		double bmi = weight / Math.pow(height/100,  2);
+		return bmi;
+	}
+	
+	// 方法封裝(getter()與setter())
 	public double getHeight() {
 		return height;
 	}
--- src/day09/UserDemo.java
+++ src/day09/UserDemo.java
@@ -7,9 +7,11 @@ public class UserDemo {
 		user1.setHeight(170.0);  // 設定物件變數
 		user1.setWeight(60.0);   // 設定物件變數
 		System.out.println(user1.getHeight() + ", " + user1.getWeight());
+		System.out.println(user1.getBmi());
 		
 		User user2 = new User(180.0, 70.0); // 建立物件時也同步設定物件變數
 		System.out.println(user2.getHeight() + ", " + user2.getWeight());
+		System.out.println(user2.getBmi());
 	}
 
 }
