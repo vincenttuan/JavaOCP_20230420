eb5f56f350e0d978cf532a53c67efe4e65283f61
MB-teacher <MB-teacher@MB-207-teacher>
2023-05-30 19:00:47 +0800

    java

---
+++ src/day09/User.java
@@ -0,0 +1,21 @@
+package day09;
+
+public class User {
+	private double height; // 身高
+	private double weight; // 體重
+	
+	// getter/setter 封裝
+	public double getHeight() {
+		return height;
+	}
+	public void setHeight(double height) {
+		this.height = height;
+	}
+	public double getWeight() {
+		return weight;
+	}
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	
+}
