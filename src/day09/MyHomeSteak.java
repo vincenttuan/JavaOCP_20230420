62826ac5322b2d3238da1092e4107b3d91c47f6d
MB-teacher <MB-teacher@MB-207-teacher>
2023-05-30 21:03:29 +0800

    java

--- src/day09/MyHomeSteak.java
+++ src/day09/MyHomeSteak.java
@@ -3,9 +3,9 @@ package day09;
 public class MyHomeSteak {
 
 	public static void main(String[] args) {
-		Steak steak1 = new Steak("菲力牛排", 350, "Medium");
-		Steak steak2 = new Steak("丁骨牛排", 550, "Medium Rare");
-		Steak steak3 = new Steak("牛小排", 450, "Medium Well");
+		Steak steak1 = new Steak("菲力牛排", 350, Doneness.MEDIUM);
+		Steak steak2 = new Steak("丁骨牛排", 550, Doneness.MEDIUM_RARE);
+		Steak steak3 = new Steak("牛小排", 450, Doneness.MEDIUM_WELL);
 		
 		System.out.println(steak1);
 		System.out.println(steak2);
