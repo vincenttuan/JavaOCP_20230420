757b35e2b427e497cef76843b9996e98ba58019f
MB-teacher <MB-teacher@MB-207-teacher>
2023-06-06 19:53:13 +0800

    java

---
+++ src/day11/DrinkStore.java
@@ -0,0 +1,17 @@
+package day11;
+
+public class DrinkStore {
+
+	public static void main(String[] args) {
+		Drink coffee = new Coffee("Latte", 65);
+		Drink blackTea = new Tea("紅茶", 15);
+		Drink greenTea = new Tea("綠茶", 17);
+		Drink orange = new Juice("柳橙汁", 70);
+		Drink watermelon = new Juice("西瓜汁", 40);
+		Drink espresso = new Espresso("Espresso", 75, 2);
+		
+		
+		
+	}
+
+}
