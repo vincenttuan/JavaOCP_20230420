d3b859c1753d808a1e4eb015b5e188ba0eae30e7
MB-teacher <MB-teacher@MB-207-teacher>
2023-06-06 20:44:06 +0800

    java

--- src/day11/DrinkStore.java
+++ src/day11/DrinkStore.java
@@ -7,7 +7,7 @@ public class DrinkStore {
 
 	public static void main(String[] args) {
 		Drink coffee = new Coffee("Latte", 65);
-		Drink blackTea = new Tea("紅茶", 15);
+		Drink blackTea = new Tea("紅茶", 16);
 		Drink greenTea = new Tea("綠茶", 17);
 		Drink orange = new Juice("柳橙汁", 70);
 		Drink watermelon = new Juice("西瓜汁", 40);
@@ -30,6 +30,9 @@ public class DrinkStore {
 				.average()
 				.orElse(0);
 		System.out.printf("平均價格: %.1f\n", averagePrice);
+		
+		// Coffee類, Tea類, Juice類 每一組各找一杯最便宜的飲料, 並算出總和
+		
 	}
 
 }
