32980c21688b691714bfe274821d3e9df3f27d32
MB-teacher <MB-teacher@MB-207-teacher>
2023-06-06 19:04:25 +0800

    java

---
+++ src/day11/Drink.java
@@ -0,0 +1,39 @@
+package day11;
+
+// 飲料: 基礎類
+public class Drink {
+	private String name;
+	private int price;
+	
+	public Drink(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	public void prepare() {
+		System.out.println("飲料準備的方式...");
+	}
+
+	@Override
+	public String toString() {
+		return "Drink [name=" + name + ", price=" + price + "]";
+	}
+	
+	
+}
