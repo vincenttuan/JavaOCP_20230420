ae9e2a6599449638ec09e8e48203ae6348e44bc2
MB-teacher <MB-teacher@MB-207-teacher>
2023-06-13 20:00:25 +0800

    java

---
+++ src/day13_2/SideDish.java
@@ -0,0 +1,21 @@
+package day13_2;
+
+public class SideDish extends Food {
+	
+	protected Food food; // 裝飾對象
+	
+	public SideDish(Food food) {
+		this.food = food;
+	}
+
+	@Override
+	public String getName() {
+		return name + " + " + food.getName();
+	}
+
+	@Override
+	public int getPrice() {
+		return price + food.price;
+	}
+	
+}
