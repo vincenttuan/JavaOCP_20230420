2bd08b97bf8fd26f56faae1165ca8cc49697581f
MB-teacher <MB-teacher@MB-207-teacher>
2023-06-13 19:27:37 +0800

    java

---
+++ src/day13/BeverageDecorator.java
@@ -0,0 +1,5 @@
+package day13;
+
+public interface BeverageDecorator extends Beverage {
+
+}
---
+++ src/day13/MilkDecorator.java
@@ -0,0 +1,20 @@
+package day13;
+
+public class MilkDecorator implements BeverageDecorator {
+	private AbstractBeverage beverage; // 裝飾目標
+	
+	public MilkDecorator(AbstractBeverage beverage) {
+		this.beverage = beverage;
+	}
+
+	@Override
+	public String getName() {
+		return "牛奶";
+	}
+
+	@Override
+	public double getCost() {
+		return beverage.getCost() + 10.0;
+	}
+	
+}
