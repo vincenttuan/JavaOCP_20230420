ad260a33ef9f7fe5de9ea3eade12d51ea769ef86
MB-teacher <MB-teacher@MB-207-teacher>
2023-06-13 19:58:34 +0800

    java

---
+++ src/day13_2/Bread.java
@@ -0,0 +1,20 @@
+package day13_2;
+
+public class Bread extends Food {
+	
+	public Bread() {
+		name = "麵包";
+		price = 40;
+	}
+	
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public int getPrice() {
+		return price;
+	}
+	
+}
