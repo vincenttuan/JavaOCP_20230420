e8cfd235c266cb2c019b624cc742055d5315845f
MB-teacher <MB-teacher@MB-207-teacher>
2023-06-15 21:07:36 +0800

    java

--- src/day14/CalculateDemo.java
+++ src/day14/CalculateDemo.java
@@ -25,11 +25,18 @@ public class CalculateDemo {
 			return x / y;
 		};
 		
+		// 利用 Lambda 語法(簡化寫法)
 		Calculate calcDiv2 = (x, y) -> x / y;
+		
+		// 利用 Lambda 語法(簡化寫法) - BMI 計算
+		Calculate calcBMI = (h, w) -> w / Math.pow(h/100, 2);
+		
+		// 利用 Lambda 語法(簡化寫法) - x 的 y 次方
 		
 		
 		System.out.println(calcDiv1.operation(10, 20));
 		System.out.println(calcDiv2.operation(10, 20));
+		System.out.println(calcBMI.operation(170, 60));
 		
 		
 	}
