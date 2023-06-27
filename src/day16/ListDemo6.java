de6e1a052d0e4aad74985b910390258884a3e629
MB-teacher <MB-teacher@MB-207-teacher>
2023-06-27 20:57:45 +0800

    java

---
+++ src/day16/ListDemo6.java
@@ -0,0 +1,16 @@
+package day16;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ListDemo6 {
+	public static void main(String[] args) {
+		List<Integer> list = Arrays.asList(1, 2, 3, 2, 1);
+		// 請刪除重複項, 使之變成 1, 2, 3
+		
+		
+		
+		
+	}
+}
