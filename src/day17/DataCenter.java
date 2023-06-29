69d617b627479feb47284b5e68e808f643b61e30
MB-teacher <MB-teacher@MB-207-teacher>
2023-06-29 19:04:38 +0800

    java

--- src/day17/DataCenter.java
+++ src/day17/DataCenter.java
@@ -3,18 +3,28 @@ package day17;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-// 資料中心
+// 資料中心(SingleTon 單例模式)
 // 定義預設的職稱與員工
 public class DataCenter {
-	public static Set<Title> titles = new LinkedHashSet<>();
+	private static DataCenter _instance = new DataCenter();
 	
-	static {
+	private Set<Title> titles = new LinkedHashSet<>();
+	
+	private DataCenter() {
 		titles.add(new Title(1, "專員"));
 		titles.add(new Title(2, "襄理"));
 		titles.add(new Title(3, "副理"));
 		titles.add(new Title(4, "經理"));
 		titles.add(new Title(5, "協理"));
 		titles.add(new Title(6, "副總"));
+	}
+	
+	public static DataCenter getInstance() {
+		return _instance;
+	}
+	
+	public Set<Title> getTitles() {
+		return titles;
 	}
 	
 }
---
+++ src/day17/Test1.java
@@ -0,0 +1,10 @@
+package day17;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// 取得所有職稱
+
+	}
+
+}
