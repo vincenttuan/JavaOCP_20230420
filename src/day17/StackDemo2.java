baf7458428469c793b96c5faf99b2ca1f8587b4f
MB-teacher <MB-teacher@MB-207-teacher>
2023-06-29 21:14:35 +0800

    java

--- src/day17/StackDemo2.java
+++ src/day17/StackDemo2.java
@@ -8,7 +8,7 @@ public class StackDemo2 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("請輸入字串: ");
-		String str = scanner.next();
+		String str = scanner.next(); // "Java"
 		
 		Stack<Character> stack = new Stack<>();
 		for(char c : str.toCharArray()) {  // "Java" -> {'J', 'a', 'v', 'a'}
