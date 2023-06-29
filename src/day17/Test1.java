c86219ee584f8526b319478a03fe9e60448c8003
MB-teacher <MB-teacher@MB-207-teacher>
2023-06-29 19:11:47 +0800

    java

--- src/day17/Test1.java
+++ src/day17/Test1.java
@@ -1,10 +1,16 @@
 package day17;
 
+import java.util.Set;
+import static day17.DataCenter.getInstance;
+
 public class Test1 {
 
 	public static void main(String[] args) {
 		// 取得所有職稱
-
+		DataCenter dataCenter = getInstance();
+		Set<Title> titles = dataCenter.getTitles();
+		System.out.println(titles);
+		
 	}
 
 }
