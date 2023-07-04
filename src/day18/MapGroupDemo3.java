06b383550c7ac8da43926a22d27e69f30db08763
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-04 21:18:47 +0800

    java

--- src/day18/MapGroupDemo3.java
+++ src/day18/MapGroupDemo3.java
@@ -6,6 +6,13 @@ import java.util.Map;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.summingLong;
+import static java.util.stream.Collectors.mapping;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toSet;
 
 public class MapGroupDemo3 {
 
@@ -28,37 +35,33 @@ public class MapGroupDemo3 {
 		
 		// 每一種水果各有幾箱 ?
 		Map<String, Long> map1 = fruits.stream()
-				.collect(Collectors.groupingBy(Fruit::getName, Collectors.counting()));
+				.collect(groupingBy(Fruit::getName, counting()));
 		System.out.println(map1);
 		
 		// 每一種水果各有顆/個 ?
 		Map<String, Long> map2 = fruits.stream()
-				.collect(Collectors.groupingBy(Fruit::getName, Collectors.summingLong(Fruit::getQty)));
+				.collect(groupingBy(Fruit::getName, summingLong(Fruit::getQty)));
 		System.out.println(map2);
 		
 		// 每一個價位各有哪些水果 ?
 		// 9.99=[apple], 19.99=[banana, papaya, pineapple], 29.99=[mango, orange]
 		// 先根據 price 分類
 		Map<Double, List<Fruit>> groupByPrice = fruits.stream()
-				.collect(Collectors.groupingBy(Fruit::getPrice));
+				.collect(groupingBy(Fruit::getPrice));
 		System.out.println(groupByPrice);
 		
 		Map<Double, String> groupByPrice2 = fruits.stream()
-				.collect(Collectors.groupingBy(Fruit::getPrice, 
-											   Collectors.mapping(Fruit::getName, 
-													   			  Collectors.joining(", "))));
+				.collect(groupingBy(Fruit::getPrice, mapping(Fruit::getName, joining(", "))));
 		System.out.println(groupByPrice2);
 		
 		Map<Double, String> groupByPrice3 = fruits.stream()
-				.collect(Collectors.groupingBy(Fruit::getPrice, 
-											   Collectors.mapping(Fruit::getName, 
-													   			  Collectors.collectingAndThen(Collectors.toSet(), 
-													   					  					   names -> String.join(", ", names)))));
+				.collect(groupingBy(Fruit::getPrice, 
+									mapping(Fruit::getName, collectingAndThen(toSet(), names -> String.join(", ", names)))));
 		System.out.println(groupByPrice3);
 		
 		Map<Double, Set<String>> groupByPrice4 = fruits.stream()
-													   .collect(Collectors.groupingBy(Fruit::getPrice, 
-															   						  Collectors.mapping(Fruit::getName, Collectors.toSet())));
+													   .collect(groupingBy(Fruit::getPrice, 
+															   			   mapping(Fruit::getName, toSet())));
 		System.out.println(groupByPrice4);
 	}
 
