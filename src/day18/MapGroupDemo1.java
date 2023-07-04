3e716783c2e06132499f1da0342a4ad59e815297
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-04 19:06:42 +0800

    java

--- src/day18/MapGroupDemo1.java
+++ src/day18/MapGroupDemo1.java
@@ -1,7 +1,12 @@
 package day18;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 // 利用 Map 進行群組與分類 (必考)
 public class MapGroupDemo1 {
@@ -14,6 +19,17 @@ public class MapGroupDemo1 {
 		
 		// 請問每一種水果各有幾個 ?
 		// apple=4, mango=3, banana=2, orange=2, papaya=1, pineapple=1
+		
+		// 解法 1:
+		Map<String, Long> map = new LinkedHashMap<>(); // 存放 "水果=出現次數" 的集合
+		Set<String> fruitKeys = new LinkedHashSet<>(fruits); // 取得所有水果名(不重複)
+		for(String key : fruitKeys) {
+			// 找到特定元素在 list 中出現的次數
+			long value = Collections.frequency(fruits, key);
+			//System.out.printf("%s=%d\n", key, value);
+			map.put(key, value);
+		}
+		System.out.println(map);
 		
 		
 	}
