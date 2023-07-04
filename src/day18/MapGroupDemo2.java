8cf68c24a48353f0688e2ec8cdfd42afdefc8555
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-04 19:56:52 +0800

    java

--- src/day18/MapGroupDemo2.java
+++ src/day18/MapGroupDemo2.java
@@ -31,8 +31,18 @@ public class MapGroupDemo2 {
 				   .sorted(Map.Entry.<String, Long>comparingByValue() // 根據 value 來排
 						   .reversed()) // 反序
 				   		   .forEachOrdered(e -> sortedMap.put(e.getKey(), e.getValue())); // 逐一將排好的元素放入到 sortedMap 集合中
-		System.out.println(sortedMap);	// 有分類有排序		
+		System.out.println(sortedMap);	// 有分類有排序
 		
+		// 解法 2:
+		Map<String, Long> sortedMap2 = unsortedMap.entrySet()
+												  .stream()
+												  .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+												  .collect(Collectors.toMap(
+														  Map.Entry::getKey, 
+														  Map.Entry::getValue,
+														  (oldEntry, newEntry) -> oldEntry, // 若元素有衝突時使用 oldValue
+														  LinkedHashMap::new)); // 因為 toMap 預設是使用 HashMap 所以我們要改為 LinkedHashMap
+		System.out.println(sortedMap2);
 	}
 	
 }
