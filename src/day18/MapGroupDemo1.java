537cefb04770c7f218ff2649aba1e0befd34b65a
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-04 18:54:43 +0800

    java

---
+++ src/day18/MapGroupDemo1.java
@@ -0,0 +1,21 @@
+package day18;
+
+import java.util.Arrays;
+import java.util.List;
+
+// 利用 Map 進行群組與分類 (必考)
+public class MapGroupDemo1 {
+	
+	public static void main(String[] args) {
+		List<String> fruits = Arrays.asList(
+				"apple", "mango", "banana", "papaya", "orange",
+				"apple", "mango", "apple", "apple", "orange",
+				"pineapple", "banana", "mango"); 
+		
+		// 請問每一種水果各有幾個 ?
+		// apple=4, mango=3, banana=2, orange=2, papaya=1, pineapple=1
+		
+		
+	}
+	
+}
