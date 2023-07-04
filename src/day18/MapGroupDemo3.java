95b36a0a41b90155cb4770e3698ace3b0a017073
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-04 20:07:03 +0800

    java

---
+++ src/day18/MapGroupDemo3.java
@@ -0,0 +1,30 @@
+package day18;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MapGroupDemo3 {
+
+	public static void main(String[] args) {
+		List<Fruit> fruits = Arrays.asList(
+				new Fruit("apple",     10, 9.99),  // 箱
+				new Fruit("mango",     20, 29.99),
+				new Fruit("banana",    10, 19.99),
+				new Fruit("papaya",    10, 19.99),
+				new Fruit("orange",    20, 29.99),
+				new Fruit("apple",     20, 9.99),
+				new Fruit("mango",     10, 29.99),
+				new Fruit("apple",     10, 9.99),
+				new Fruit("apple",     30, 9.99),
+				new Fruit("orange",    10, 29.99),
+				new Fruit("pineapple", 20, 19.99),
+				new Fruit("banana",    20, 19.99),
+				new Fruit("mango",     10, 29.99)
+		);
+		
+		// 每一種水果各有幾箱
+		
+
+	}
+
+}
