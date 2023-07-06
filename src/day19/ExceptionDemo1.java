196c694cbca8efbe5d58f84154d41d12740a55fd
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-06 19:58:46 +0800

    java

--- src/day19/ExceptionDemo1.java
+++ src/day19/ExceptionDemo1.java
@@ -3,16 +3,22 @@ package day19;
 public class ExceptionDemo1 {
 
 	public static void main(String[] args) {
-		String data = "一二三";
 		try {
-			int number = Integer.parseInt(data);
-			System.out.println(number);
+			String[] data = {"123", "一二三"};
+			int number = Integer.parseInt(data[2]);
+			System.out.println("資料轉換成功");
+			System.out.println("number = " + number);
+		} catch (NullPointerException e) {
+			System.out.println("data 必須要有資料");
 		} catch (NumberFormatException e) {
 			//System.out.println("錯誤資訊:" + e);
 			//System.out.println("錯誤原因:" + e.getMessage());
 			//e.printStackTrace(System.err); // 印出詳細錯誤
 			System.out.println("資料轉換錯誤, 請重新輸入正確資料!");
+		} catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("陣列維度超過最大值");
 		}
+		
 		System.out.println("程式正常結束 !");
 		
 	}
