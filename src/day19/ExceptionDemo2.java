d9903cf465b929019623622a924066e3edb9d081
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-06 20:51:41 +0800

    java

--- src/day19/ExceptionDemo2.java
+++ src/day19/ExceptionDemo2.java
@@ -1,5 +1,6 @@
 package day19;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ExceptionDemo2 {
@@ -11,13 +12,18 @@ public class ExceptionDemo2 {
 	public static void play() {
 		// BMI 計算系統
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("請輸入身高cm: ");
-		double h = scanner.nextDouble();
-		System.out.print("請輸入體重kg: ");
-		double w = scanner.nextDouble();
+		try {
+			System.out.print("請輸入身高cm: ");
+			double h = scanner.nextDouble();
+			System.out.print("請輸入體重kg: ");
+			double w = scanner.nextDouble();
+			double bmi = w/Math.pow(h/100, 2);
+			System.out.printf("BMI = %.2f\n", bmi);
+		} catch (InputMismatchException e) {
+			System.out.println("輸入資料不正確請重新輸入");
+			play(); // 遞迴呼叫
+		}
 		
-		double bmi = w/Math.pow(h/100, 2);
-		System.out.printf("BMI = %.2f\n", bmi);
 	}
 	
 
