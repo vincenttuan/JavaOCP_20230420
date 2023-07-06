a5b26a9acd9bd9dbfaaf18130fbdca6c99db2978
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-06 19:46:56 +0800

    java

--- src/day19/ExceptionDemo1.java
+++ src/day19/ExceptionDemo1.java
@@ -3,9 +3,17 @@ package day19;
 public class ExceptionDemo1 {
 
 	public static void main(String[] args) {
-		String data = "123";
-		int number = Integer.parseInt(data);
-		System.out.println(number);
+		String data = "一二三";
+		try {
+			int number = Integer.parseInt(data);
+			System.out.println(number);
+		} catch (NumberFormatException e) {
+			//System.out.println("錯誤資訊:" + e);
+			//System.out.println("錯誤原因:" + e.getMessage());
+			//e.printStackTrace(System.err); // 印出詳細錯誤
+			System.out.println("資料轉換錯誤, 請重新輸入正確資料!");
+		}
+		System.out.println("程式正常結束 !");
 		
 	}
 
