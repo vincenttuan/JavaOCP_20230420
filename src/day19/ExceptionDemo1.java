a2c1a7369450f0fba36186ae34b456bfe8acd53e
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-06 19:41:13 +0800

    java

---
+++ src/day19/ExceptionDemo1.java
@@ -0,0 +1,12 @@
+package day19;
+
+public class ExceptionDemo1 {
+
+	public static void main(String[] args) {
+		String data = "123";
+		int number = Integer.parseInt(data);
+		System.out.println(number);
+		
+	}
+
+}
