321df80dc61419f918997bea0bb90d0aeeeefd2c
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-11 19:37:11 +0800

    java

---
+++ src/day20/Base64Demo.java
@@ -0,0 +1,20 @@
+package day20;
+
+import java.util.Base64;
+
+public class Base64Demo {
+
+	public static void main(String[] args) {
+		String clearText = "1234"; // 明文
+		System.out.println("明文:" + clearText);
+		// 編碼
+		String encodeText = Base64.getEncoder().encodeToString(clearText.getBytes());
+		System.out.println("編碼:" + encodeText);
+		// 解碼
+		byte[] decodeBytes = Base64.getDecoder().decode(encodeText);
+		String decodeText = new String(decodeBytes);
+		System.out.println("解碼:" + decodeText);
+
+	}
+
+}
