c24505d8ddc4f8a1b5e7264336bd4a7c42876d45
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-11 19:10:44 +0800

    java

---
+++ src/day20/User.java
@@ -0,0 +1,38 @@
+package day20;
+
+import java.util.Base64;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class User {
+	
+	// 所有 user 的登入帳號
+	public static Map<String, String> users = new LinkedHashMap<>();
+	
+	static {
+		users.put("John", Base64.getEncoder().encodeToString("1234".getBytes())); // 將 1234 進行 Base64 編碼
+		users.put("Mary", Base64.getEncoder().encodeToString("5678".getBytes())); // 將 5678 進行 Base64 編碼
+		users.put("Rose", Base64.getEncoder().encodeToString("abcd".getBytes())); // 將 abcd 進行 Base64 編碼
+	}
+	
+	// 登入
+	public static boolean loginCheck(String inputUsername, String inputPassword) {
+		// 1. 先確認是否有此帳號 ? 若有帳號就可以得到 password
+		String encodePassword = users.get(inputUsername);
+		if(encodePassword == null) { // 無此帳號
+			return false;
+		}
+		// 2. 比對 password, 因為傳進來的 password 是明文所以必須轉 base64 進行比對
+		String inputPasswordEncode = Base64.getEncoder().encodeToString(inputPassword.getBytes());
+		if(encodePassword.equals(inputPasswordEncode)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
