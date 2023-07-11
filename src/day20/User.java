9f14f9aa5e5d92bf456b9235739f8d7904cd64fa
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-11 19:24:25 +0800

    java

--- src/day20/User.java
+++ src/day20/User.java
@@ -16,24 +16,31 @@ public class User {
 	}
 	
 	// 登入
-	public static boolean loginCheck(String inputUsername, String inputPassword) {
+	public static boolean loginCheck(String inputUsername, String inputPassword) throws LoginException {
 		// 1. 先確認是否有此帳號 ? 若有帳號就可以得到 password
 		String encodePassword = users.get(inputUsername);
 		if(encodePassword == null) { // 無此帳號
-			return false;
+			LoginException loginException = new LoginException("無此帳號");
+			throw loginException; // 取代 return false;
 		}
 		// 2. 比對 password, 因為傳進來的 password 是明文所以必須轉 base64 進行比對
 		String inputPasswordEncode = Base64.getEncoder().encodeToString(inputPassword.getBytes());
 		if(encodePassword.equals(inputPasswordEncode)) {
 			return true;
 		} else {
-			return false;
+			LoginException loginException = new LoginException("密碼錯誤");
+			throw loginException; // 取代 return false;
 		}
 	}
 	
 	public static void main(String[] args) {
 		System.out.println(User.users);
-		System.out.println(User.loginCheck("John", "1234"));
+		try {
+			System.out.println(User.loginCheck("John", "1234"));
+		} catch (LoginException e) {
+			System.out.println(e.getMessage());
+			e.howToDo();
+		}
 	}
 	
 }
