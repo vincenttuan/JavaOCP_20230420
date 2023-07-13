8a3685a0dd1d68dfabdddf50e37ba1c1584e4a82
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-13 21:35:18 +0800

    java

--- src/day21/JobMain4.java
+++ src/day21/JobMain4.java
@@ -2,11 +2,23 @@ package day21;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class JobMain4 {
 	public static void main(String[] args) {
 		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
-		// 請利用5條執行緒分別算出每個元素的平方和
+		// 請利用5條執行緒分別算出每個元素的平方
+		Set<Runnable> jobs = numbers.stream()
+				.map(num -> (Runnable)()->System.out.println(num * num))
+				.collect(Collectors.toSet());
+		
+		Set<Thread> threads = jobs.stream()
+				.map(job -> new Thread(job))
+				.collect(Collectors.toSet());
+		
+		threads.forEach(Thread::start);
+		
 		
 	}
 }
