ec06b8e9fa0aa15f70f01ab53b8dc741cc4acf7a
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-13 21:28:02 +0800

    java

---
+++ src/day21/JobMain4.java
@@ -0,0 +1,12 @@
+package day21;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class JobMain4 {
+	public static void main(String[] args) {
+		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+		// 請利用5條執行緒分別算出每個元素的平方和
+		
+	}
+}
