0623a40ad09ea8a0e264d836917ddfff02519de5
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-13 20:05:00 +0800

    java

---
+++ src/day21/JobMain.java
@@ -0,0 +1,10 @@
+package day21;
+
+// 主程式
+public class JobMain {
+	public static void main(String[] args) {
+		String threadName = Thread.currentThread().getName();
+		System.out.printf("主執行緒名稱: %s\n", threadName);
+		
+	}
+}
