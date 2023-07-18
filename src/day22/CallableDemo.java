266b33099baf2cd4bbc87a0db6edacfe94ef6e6c
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-18 19:52:26 +0800

    java

---
+++ src/day22/CallableDemo.java
@@ -0,0 +1,31 @@
+package day22;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+public class CallableDemo {
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		Runnable runnable = () -> {
+			System.out.println("Welcome Runnable");
+		};
+		
+		Thread t1 = new Thread(runnable);
+		t1.start();
+		
+		// ------------------------------------------
+		Callable<Integer> callable = () -> {
+			System.out.println("Welcome Callable");
+			return 77;
+		};
+		
+		FutureTask<Integer> futureTask = new FutureTask<>(callable);
+		Thread t2 = new Thread(futureTask);
+		t2.start();
+		System.out.println(futureTask.get());
+		
+		
+	}
+
+}
