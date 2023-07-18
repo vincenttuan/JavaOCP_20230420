3e7b0e413762097cb9d976e8a598fb6b0b6fbbfa
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-18 19:12:58 +0800

    java

---
+++ src/day22/Lottery2.java
@@ -0,0 +1,15 @@
+package day22;
+
+public class Lottery2 {
+	
+	static int lottoNumber;
+	public static void main(String[] args) {
+		// 主執行緒負責印出"開始"與"結束"
+		// 子1執行緒負責印出"開場"與"公布獎號"
+		// 子2執行緒負責"搖獎"
+		
+		
+
+	}
+
+}
