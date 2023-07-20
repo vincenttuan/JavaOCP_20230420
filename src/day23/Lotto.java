d5cbf8af0965432a4ffc1cef37bc8502b05eb863
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-20 20:59:41 +0800

    java

--- src/day23/Lotto.java
+++ src/day23/Lotto.java
@@ -3,18 +3,19 @@ package day23;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
-import java.util.concurrent.Callable;
 
-public class Lotto implements Callable {
+public class Lotto implements Runnable {
 	private SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
 	
 	@Override
-	public Object call() throws InterruptedException {
+	public void run() {
 		System.out.print("工作開始時間:" + sdf.format(new Date()));
-		Thread.sleep(new Random().nextInt(2500));
+		try {
+			Thread.sleep(new Random().nextInt(2500));
+		} catch (Exception e) {
+		}
 		System.out.print(" 樂透數字 " + new Random().nextInt(100) + " ");
 		System.out.println("工作結束時間:" + sdf.format(new Date()));
-		return null;
 	}
 	
 }
---
+++ src/day23/LottoScheduledDemo.java
@@ -0,0 +1,21 @@
+package day23;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class LottoScheduledDemo {
+
+	public static void main(String[] args) throws Exception {
+		
+		ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+		
+		int initDelay = 1;
+		int delay = 2;
+		service.scheduleWithFixedDelay(new Lotto(), initDelay, delay, TimeUnit.SECONDS);
+		
+		
+
+	}
+
+}
