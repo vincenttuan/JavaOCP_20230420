eb549faf550ea3f2adc96cfadc427ad0e7dcb55d
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-20 19:20:31 +0800

    java

---
+++ src/day23/MyExecutor.java
@@ -0,0 +1,13 @@
+package day23;
+
+import java.util.concurrent.Executor;
+
+public class MyExecutor implements Executor {
+
+	@Override
+	public void execute(Runnable command) {
+		Thread t = new Thread(command);
+		t.start();
+	}
+
+}
