a4f7ead43de239ec7baa405c9ed65c4226ac8fb6
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-25 19:01:44 +0800

    java

--- src/day24/ReadFile.java
+++ src/day24/ReadFile.java
@@ -1,16 +1,28 @@
 package day24;
 
 import java.io.File;
+import java.io.IOException;
 
 public class ReadFile {
 
 	public static void main(String[] args) {
-		File file = new File("src/day24/employee.txt");
+		File file = new File("src/day24/age.txt");
 		// 檔案是否存在 ?
 		if(file.exists()) {
-			System.out.println("檔案存在");
+			System.out.println(file.getName() + " 檔案存在");
 		} else {
-			System.out.println("檔案不存在");
+			System.out.println(file.getName() + " 檔案不存在");
+			// 建立檔案
+			try {
+				boolean isOk = file.createNewFile();
+				if(isOk) {
+					System.out.println(file.getName() + " 建立成功 !");
+				} else {
+					System.out.println(file.getName() + " 建立不成功 !");
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 
 	}
