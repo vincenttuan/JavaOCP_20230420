67575479d9311246c4bf31b1e2f1fb51c1a2a759
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-25 20:02:30 +0800

    java

--- src/day24/ReadFileContent.java
+++ src/day24/ReadFileContent.java
@@ -1,7 +1,6 @@
 package day24;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
--- src/day24/ReadFileContent3.java
+++ src/day24/ReadFileContent3.java
@@ -1,9 +1,6 @@
 package day24;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.IntSummaryStatistics;
 import java.util.List;
 
