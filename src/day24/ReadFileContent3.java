8298639998f541e94d101d41810e396fd729f6e3
MB-teacher <MB-teacher@MB-207-teacher>
2023-07-25 19:59:51 +0800

    java

--- src/day24/ReadFileContent3.java
+++ src/day24/ReadFileContent3.java
@@ -1,11 +1,19 @@
 package day24;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+
 public class ReadFileContent3 {
 
-	public static void main(String[] args) {
-		// 請計算 age.txt 的平均年齡 ?
-		
-
+	public static void main(String[] args) throws IOException {
+		// 請計算 age.txt 的平均年齡, 最大與最小年齡 ?
+		List<String> lines = Util.getLines("src/day24/age.txt");
+		IntSummaryStatistics stat = lines.stream().mapToInt(Util::getValue).summaryStatistics();
+		System.out.printf("平均年齡: %.1f, 最大: %d 最小: %d\n", stat.getAverage(), stat.getMax(), stat.getMin());
 	}
 
 }
--- src/day24/Util.java
+++ src/day24/Util.java
@@ -1,7 +1,18 @@
 package day24;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Util {
 	public static int getValue(String line) {
 		return Integer.parseInt(line.split(" ")[1]);
+	}
+	
+	public static List<String> getLines(String url) throws IOException {
+		Path path = Paths.get(url);
+		return Files.readAllLines(path);
 	}
 }
