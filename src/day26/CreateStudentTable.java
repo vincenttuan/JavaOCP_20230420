37da761ed81e4df236ea0a5ca057245b15e11cdf
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-01 19:24:49 +0800

    java

--- src/day26/CreateStudentTable.java
+++ src/day26/CreateStudentTable.java
@@ -1,8 +1,13 @@
 package day26;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class CreateStudentTable {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException {
 		// MySQL 資料連線設定
 		String dbUrl = "jdbc:mysql://localhost:3306/demo?useSSL=false&serverTimezone=Asia/Taipei";
 		String username = "root";
@@ -21,9 +26,22 @@ public class CreateStudentTable {
 		
 		// 1. 註冊 driver (JDBC 4.0之後可以不用寫)
 		Class.forName(driverName);
+		
+		
+		try(Connection conn = DriverManager.getConnection(dbUrl, username, password);
+			Statement stmt = conn.createStatement();) {
+			// 執行一個新增資料表的 sql, 使用 executeUpdate
+			// result = 1 表示新增成功, result = 0 表示資料表已經存在不用再新增
+			int result = stmt.executeUpdate(sql);
 			
-		try() {
+			if(result == 1) {
+				System.out.println("Student 資料表新增成功");
+			} else {
+				System.out.println("Student 表示資料表已經存在不用再新增");
+			}
 			
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 
 	}
