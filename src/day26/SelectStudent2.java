6b2e7efb8670880563dfc5e1f9a172e726d9ae4c
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-01 21:16:28 +0800

    java

---
+++ src/day26/SelectStudent2.java
@@ -0,0 +1,74 @@
+package day26;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SelectStudent2 {
+	public static void main(String[] args) {
+		// MySQL 資料連線設定
+		String dbUrl = "jdbc:mysql://localhost:3306/demo?allowPublicKeyRetrieval=true&serverTimezone=Asia/Taipei";
+		String username = "root";
+		String password = "12345678";
+		String driverName = "com.mysql.cj.jdbc.Driver";
+		
+		// 查詢 student 資料並排名次
+		
+		try(Connection conn = DriverManager.getConnection(dbUrl, username, password);
+			CallableStatement stmt = conn.prepareCall("{call myrank()}");) {
+			
+			ResultSet rs = stmt.executeQuery();
+			// 取得資料表欄位名
+			ResultSetMetaData md = rs.getMetaData();
+			for(int i=1; i<=md.getColumnCount();i++) {
+				System.out.printf("%10s", md.getColumnLabel(i));
+			}
+			
+			// 分隔線
+			System.out.println();
+			for(int i=0;i<55;i++) {
+				System.out.print("-");
+			}
+			System.out.println();
+			
+			// 取得資料表內容
+			List<Student> students = new ArrayList<>();
+			while (rs.next()) {
+				int myrank = rs.getInt("myrank");
+				int id = rs.getInt("id");
+				int score1 = rs.getInt("score1");
+				int score2 = rs.getInt("score2");
+				int score3 = rs.getInt("score3");
+				int score4 = rs.getInt("score4");
+				
+				Student student = new Student();
+				student.setMyrank(myrank);
+				student.setId(id);
+				student.setScore1(score1);
+				student.setScore2(score2);
+				student.setScore3(score3);
+				student.setScore4(score4);
+				
+				students.add(student);
+			}
+			
+			//System.out.println(students);
+			for(Student student : students) {
+				System.out.printf("%10d%10d%10d%10d%10d%10d\n", 
+								  student.getMyrank(), student.getId(), 
+								  student.getScore1(), student.getScore2(), student.getScore3(), student.getScore4());
+			}
+			
+			
+		} catch (SQLException e) {
+			// TODO: handle exception
+		}
+		
+	}
+}
--- src/day26/Student.java
+++ src/day26/Student.java
@@ -1,12 +1,18 @@
 package day26;
 
 public class Student {
+	private Integer myrank;
 	private Integer id;
 	private Integer score1;
 	private Integer score2;
 	private Integer score3;
 	private Integer score4;
-	
+	public Integer getMyrank() {
+		return myrank;
+	}
+	public void setMyrank(Integer myrank) {
+		this.myrank = myrank;
+	}
 	public Integer getId() {
 		return id;
 	}
@@ -39,9 +45,10 @@ public class Student {
 	}
 	@Override
 	public String toString() {
-		return "Student [id=" + id + ", score1=" + score1 + ", score2=" + score2 + ", score3=" + score3 + ", score4="
-				+ score4 + "]";
+		return "Student [myrank=" + myrank + ", id=" + id + ", score1=" + score1 + ", score2=" + score2 + ", score3="
+				+ score3 + ", score4=" + score4 + "]";
 	}
+	
 	
 	
 	
