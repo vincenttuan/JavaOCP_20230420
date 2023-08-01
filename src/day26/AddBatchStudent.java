799540db57c8d188ff012b3898c099418b11cb6d
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-01 20:03:17 +0800

    java

--- src/day26/AddBatchStudent.java
+++ src/day26/AddBatchStudent.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Arrays;
 import java.util.List;
 
 // 批次新增
@@ -15,7 +19,40 @@ public class AddBatchStudent {
 		List<String> lines = Files.readAllLines(filePath);
 		System.out.println("資料筆數: " + lines.size());
 		System.out.println("資料內容: " + lines);
+		// 2. MySQL 連線設定
+		String dbUrl = "jdbc:mysql://localhost:3306/demo?allowPublicKeyRetrieval=true&serverTimezone=Asia/Taipei";
+		String username = "root";
+		String password = "12345678";
+		String driverName = "com.mysql.cj.jdbc.Driver";
 		
+		// 3. 新增 student 紀錄的 sql
+		String sql = "insert into student(score1, score2, score3, score4) values (?, ?, ?, ?)";
+		
+		try(Connection conn = DriverManager.getConnection(dbUrl, username, password);
+			PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			
+			pstmt.clearBatch();
+			
+			for(String line : lines) {
+				String[] arr = line.split(",");
+				int score1 = Integer.parseInt(arr[0].trim());
+				int score2 = Integer.parseInt(arr[0].trim());
+				int score3 = Integer.parseInt(arr[0].trim());
+				int score4 = Integer.parseInt(arr[0].trim());
+				pstmt.setInt(1, score1);
+				pstmt.setInt(2, score2);
+				pstmt.setInt(3, score3);
+				pstmt.setInt(4, score4);
+				pstmt.addBatch(sql);
+			}
+			
+			int[] resultArray = pstmt.executeBatch();
+			System.out.println("批次新增完成");
+			System.out.println(Arrays.toString(resultArray));
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 	}
 
